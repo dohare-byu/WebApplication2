a9df66ecbcd374a2f899afe600f4e562dc5315fc
Daniel O'Hare <71663427+dohare-byu@users.noreply.github.com>
2021-04-02 17:40:56 -0600

    Add project files.

---
+++ Migrations/20210402233500_initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication2.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    Director = table.Column<string>(type: "TEXT", nullable: false),
+                    Rating = table.Column<string>(type: "TEXT", nullable: false),
+                    Edited = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Lent = table.Column<string>(type: "TEXT", nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Movie //this is the class for movies 
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; } //primary key
+
+        [Required]
+        public string Category { get; set; } //attributes of the class here and below
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string Lent { get; set; }
+
+        [MaxLength(25)]
+        public string Notes { get; set; }
+    }
+}
